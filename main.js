74c45c1c44bdc3fcd824d4197ed2b8a02e10ec73
makzuu <mr.greenmousth@gmail.com>
2024-06-13 20:31:13 -0400

    Update main

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import { argv, exit } from 'process'
+
+function main() {
+    if (argv.length !== 3) {
+        console.log("Error: expecting one command line argument <baseUrl>, zero were given")
+        exit(1)
+    }
+    const baseUrl = argv[2]
+    console.log(`base url: ${baseUrl}`)
+}
+
+main()
